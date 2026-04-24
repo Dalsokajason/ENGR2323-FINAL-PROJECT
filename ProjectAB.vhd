65deef4bf1780c73845aedbe2033c563f50ca02a
Jason Do <153405342+Dalsokajason@users.noreply.github.com>
2026-04-24 13:43:18 -0400

    Refactor ProjectAB to add Next_State and temp hex signals
    Refactor ProjectAB VHDL code to include Next_State input and temporary hex values for better state management and display handling.

--- ProjectAB.vhd
+++ ProjectAB.vhd
@@ -7,10 +7,11 @@ Port (Clock, Resetn : IN STD_LOGIC;
 		Col : IN STD_LOGIC_VECTOR(1 downto 0);
 		Adjust_Up: IN STD_LOGIC;
 		Adjust_Down: IN STD_LOGIC;
+		Next_State: IN STD_LOGIC;
 		Item_Price : OUT STD_LOGIC_VECTOR(4 downto 0);
 		Item_Slot : OUT STD_LOGIC_VECTOR(4 downto 0);
-		Hex_Col	: OUT STD_LOGIC_VECTOR(6 downto 0);
-		Hex_Row	: OUT STD_LOGIC_VECTOR(6 downto 0);
+		Hex_COL	: OUT STD_LOGIC_VECTOR(6 downto 0);
+		Hex_ROW	: OUT STD_LOGIC_VECTOR(6 downto 0);
 		HEX_COUNTER1: OUT STD_LOGIC_VECTOR(6 downto 0);
 		HEX_COUNTER2: OUT STD_LOGIC_VECTOR(6 downto 0);
 		HEX_COUNTER3: OUT STD_LOGIC_VECTOR(6 downto 0);
@@ -78,6 +79,12 @@ SIGNAL CHANGE_ONES		: INTEGER RANGE 0 TO 9;
 SIGNAL CHANGE_TENTHS		: INTEGER RANGE 0 TO 9;
 SIGNAL CHANGE_HUNDREDTHS: INTEGER RANGE 0 TO 9;
 
+--Temporary hex values
+SIGNAL TEMP_HEX_COUNTER1 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL TEMP_HEX_COUNTER2 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL TEMP_HEX_COUNTER3 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL TEMP_HEX_ROW : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL TEMP_HEX_COL : STD_LOGIC_VECTOR(6 DOWNTO 0);
 
 --Variables that set how much each items in a category costs values will always be x10^2 of actual displayed price
 --default values
@@ -97,26 +104,22 @@ SIGNAL PRICE_JUICE    : INTEGER RANGE 0 TO 999 := 210;  -- $2.10
 --100 DIET SODA
 --101 JUICE 
 
---Keep track of previous inputs 111 and 11 represent no input
-SIGNAL PREV_ROW : STD_LOGIC_VECTOR(2 downto 0) := "111";
-SIGNAL PREV_COL : STD_LOGIC_VECTOR(1 downto 0) := "11";
-SIGNAL PREV_TENDER: INTEGER RANGE 0 TO 999 := 111;
 --Function to convert from an integer digit to the 7seg hex digit assignment
 --ex: 8 -> "0000000"
 FUNCTION convertToHex(digit : INTEGER) RETURN STD_LOGIC_VECTOR IS
 		BEGIN
 		CASE digit IS
-		  WHEN 0 => RETURN HEX_ZERO;
-		  WHEN 1 => RETURN HEX_ONE;
-		  WHEN 2 => RETURN HEX_TWO;
-		  WHEN 3 => RETURN HEX_THREE;
-		  WHEN 4 => RETURN HEX_FOUR;
-		  WHEN 5 => RETURN HEX_FIVE;
-		  WHEN 6 => RETURN HEX_SIX;
-		  WHEN 7 => RETURN HEX_SEVEN;
-		  WHEN 8 => RETURN HEX_EIGHT;
-		  WHEN 9 => RETURN HEX_NINE;
-		  WHEN OTHERS => RETURN HEX_OFF;
+		  WHEN 0 => RETURN "1000000";
+		  WHEN 1 => RETURN "1111001";
+		  WHEN 2 => RETURN "0100100";
+		  WHEN 3 => RETURN "0110000";
+		  WHEN 4 => RETURN "0011001";
+		  WHEN 5 => RETURN "0010010";
+		  WHEN 6 => RETURN "0000010";
+		  WHEN 7 => RETURN "1111000";
+		  WHEN 8 => RETURN "0000000";
+		  WHEN 9 => RETURN "0011000";
+		  WHEN OTHERS => RETURN "1111111";
 		END CASE;
 END FUNCTION;
 	
@@ -126,15 +129,11 @@ PROCESS(Clock, Resetn)
 	BEGIN 
 		IF Resetn = '0' THEN
 			STATE <= STATE0;
-			PREV_ROW <= "111";
-			PREV_COL <= "11";
 		ELSIF (Rising_Edge(Clock)) THEN
 			CASE STATE IS
 				WHEN STATE0 =>
-					PREV_ROW <= Row;
-					PREV_COL <= Col;
 					
-					IF (Row = PREV_ROW AND Col = PREV_COL) THEN
+					IF (Next_State = '1') THEN
 						STATE <= STATE1;
 					END IF;
 					--First Column	
@@ -228,22 +227,22 @@ PROCESS(Clock, Resetn)
 					END IF;
 					
 					CASE Col IS
-						WHEN "00" => HEX_COL <= HEX_ZERO;
-						WHEN "01" => HEX_COL <= HEX_ONE;
-						WHEN "10" => HEX_COL <= HEX_TWO;
-						WHEN "11" => HEX_COL <= HEX_THREE;
-						WHEN OTHERS => HEX_COL <= HEX_OFF;
+						WHEN "00" => TEMP_HEX_COL <= HEX_ZERO;
+						WHEN "01" => TEMP_HEX_COL <= HEX_ONE;
+						WHEN "10" => TEMP_HEX_COL <= HEX_TWO;
+						WHEN "11" => TEMP_HEX_COL <= HEX_THREE;
+						WHEN OTHERS => TEMP_HEX_COL <= HEX_OFF;
 					END CASE;
 
 					CASE Row IS
-						WHEN "000" => HEX_ROW <= HEX_A;
-						WHEN "001" => HEX_ROW <= HEX_B;
-						WHEN "010" => HEX_ROW <= HEX_C;
-						WHEN "011" => HEX_ROW <= HEX_D;
-						WHEN "100" => HEX_ROW <= HEX_E;
-						WHEN "101" => HEX_ROW <= HEX_F;
-						WHEN "110" => HEX_ROW <= HEX_G;
-						WHEN OTHERS => HEX_ROW <= HEX_OFF;
+						WHEN "000" => TEMP_HEX_ROW <= HEX_A;
+						WHEN "001" => TEMP_HEX_ROW <= HEX_B;
+						WHEN "010" => TEMP_HEX_ROW <= HEX_C;
+						WHEN "011" => TEMP_HEX_ROW <= HEX_D;
+						WHEN "100" => TEMP_HEX_ROW <= HEX_E;
+						WHEN "101" => TEMP_HEX_ROW <= HEX_F;
+						WHEN "110" => TEMP_HEX_ROW <= HEX_G;
+						WHEN OTHERS => TEMP_HEX_ROW <= HEX_OFF;
 					END CASE;
 					
 					--000 CANDY
@@ -267,41 +266,54 @@ PROCESS(Clock, Resetn)
 					PRICE_TENTHS <= (PRICE MOD 100) / 10;
 					PRICE_HUNDREDTHS <= PRICE MOD 10;
 					--convert each digit to hex and send to 7 segment display
-					HEX_COUNTER1 <= convertToHex(PRICE_ONES);
-					HEX_COUNTER2 <= convertToHex(PRICE_TENTHS);
-					HEX_COUNTER3 <= convertToHex(PRICE_HUNDREDTHS);
+					TEMP_HEX_COUNTER1 <= convertToHex(PRICE_ONES);
+					TEMP_HEX_COUNTER2 <= convertToHex(PRICE_TENTHS);
+					TEMP_HEX_COUNTER3 <= convertToHex(PRICE_HUNDREDTHS);
 					
 				WHEN STATE1 => 
-					IF (Rising_Edge(Adjust_Up)) THEN
+					IF (Next_State = '1') THEN
+						STATE <= STATE2
+					IF (Adjust_Up = '1') THEN
 						TENDER <= TENDER + 5;
-						--grab each digit of TENDER
-						TENDER_ONES <= TENDER / 100;
-						TENDER_TENTHS <= (TENDER MOD 100) / 10;
-						TENDER_HUNDREDTHS <= TENDER MOD 10;
-						--convert each digit to hex and send to 7 segment display
-						HEX_COUNTER1 <= convertToHex(TENDER_ONES);
-						HEX_COUNTER2 <= convertToHex(TENDER_TENTHS);
-						HEX_COUNTER3 <= convertToHex(TENDER_HUNDREDTHS);
 						
-					ELSIF (Rising_Edge(Adjust_Down)) THEN
+					ELSIF (Adjust_Down = '1') THEN
 						TENDER <= TENDER - 5;
-						--grab each digit of TENDER
-						TENDER_ONES <= TENDER / 100;
-						TENDER_TENTHS <= (TENDER MOD 100) / 10;
-						TENDER_HUNDREDTHS <= TENDER MOD 10;
-						--convert each digit to hex and send to 7 segment display
-						HEX_COUNTER1 <= convertToHex(TENDER_ONES);
-						HEX_COUNTER2 <= convertToHex(TENDER_TENTHS);
-						HEX_COUNTER3 <= convertToHex(TENDER_HUNDREDTHS);
 					END IF;
 					
+					--grab each digit of TENDER
+					TENDER_ONES <= TENDER / 100;
+					TENDER_TENTHS <= (TENDER MOD 100) / 10;
+					TENDER_HUNDREDTHS <= TENDER MOD 10;
+					--convert each digit to hex and send to 7 segment display
+					TEMP_HEX_COUNTER1 <= convertToHex(TENDER_ONES);
+					TEMP_HEX_COUNTER2 <= convertToHex(TENDER_TENTHS);
+					TEMP_HEX_COUNTER3 <= convertToHex(TENDER_HUNDREDTHS);
+				WHEN STATE2 =>
+					IF (Next_State = '1') THEN
+						STATE <= STATE0;
+					END IF;
 					
-						
+					CHANGE <= TENDER - PRICE;
+					
+					CHANGE_ONES <= CHANGE / 100;
+					CHANGE_TENTHS <= (CHANGE MOD 100) / 10;
+					CHANGE_HUNDREDTHS <= CHANGE MOD 10;
+					--convert each digit to hex and send to 7 segment display
+					TEMP_HEX_COUNTER1 <= convertToHex(CHANGE_ONES);
+					TEMP_HEX_COUNTER2 <= convertToHex(CHANGE_TENTHS);
+					TEMP_HEX_COUNTER3 <= convertToHex(CHANGE_HUNDREDTHS);
+				END CASE;	
 					
 		END IF;
 	
 	HEX3 <= HEX_OFF;
 	
+	HEX_COUNTER1 <= TEMP_HEX_COUNTER1;
+	HEX_COUNTER2 <= TEMP_HEX_COUNTER2;
+	HEX_COUNTER3 <= TEMP_HEX_COUNTER3;
+
+	HEX_ROW <= TEMP_HEX_ROW;
+	HEX_COL <= TEMP_HEX_COL;
 	
 	
 		
