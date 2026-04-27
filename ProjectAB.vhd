0cc29da02fd2514181b260c66e62c48b148b77dc
Jason Do <153405342+Dalsokajason@users.noreply.github.com>
2026-04-27 13:24:43 -0400

    Add state LED indicators to ProjectAB
    Added LED indicators for each state in the ProjectAB entity.

--- ProjectAB.vhd
+++ ProjectAB.vhd
@@ -15,7 +15,10 @@ Port (Clock, Resetn : IN STD_LOGIC;
 		HEX_COUNTER1: OUT STD_LOGIC_VECTOR(6 downto 0);
 		HEX_COUNTER2: OUT STD_LOGIC_VECTOR(6 downto 0);
 		HEX_COUNTER3: OUT STD_LOGIC_VECTOR(6 downto 0);
-		HEX3: OUT STD_LOGIC_VECTOR(6 downto 0) --USED TO TURN OFF THE HEX3 PROBLEM
+		HEX3: OUT STD_LOGIC_VECTOR(6 downto 0); --USED TO TURN OFF THE HEX3 PROBLEM
+		STATE0_LED: OUT STD_LOGIC;
+		STATE1_LED: OUT STD_LOGIC;
+		STATE2_LED: OUT STD_LOGIC
 		);
 END ProjectAB;
 
@@ -76,9 +79,24 @@ SIGNAL PRICE_CHIPS    : INTEGER RANGE 0 TO 999 := 300;  -- $3.00
 SIGNAL PRICE_NUTS     : INTEGER RANGE 0 TO 999 := 250;  -- $2.50
 SIGNAL PRICE_SODA     : INTEGER RANGE 0 TO 999 := 175;  -- $1.75
 SIGNAL PRICE_DIETSODA : INTEGER RANGE 0 TO 999 := 125;  -- $1.25
-SIGNAL PRICE_JUICE    : INTEGER RANGE 0 TO 999 := 210;  -- $2.10
+SIGNAL PRICE_JUICE    : INTEGER RANGE 0 TO 999 := 210;  -- $2.10 -- MAKE GATEGORY FOR 
 
+--integers for doing transaction math
+SIGNAL PRICE 				: INTEGER RANGE 0 TO 995;
+SIGNAL TENDER				: INTEGER RANGE 0 TO 995 := 3;
+SIGNAL CHANGE				: INTEGER RANGE 0 TO 995;
 
+SIGNAL PRICE_ONES			: INTEGER RANGE 0 TO 9;
+SIGNAL PRICE_TENTHS		: INTEGER RANGE 0 TO 9;
+SIGNAL PRICE_HUNDREDTHS	: INTEGER RANGE 0 TO 9;
+
+SIGNAL TENDER_ONES		: INTEGER RANGE 0 TO 9;
+SIGNAL TENDER_TENTHS		: INTEGER RANGE 0 TO 9;
+SIGNAL TENDER_HUNDREDTHS: INTEGER RANGE 0 TO 9;
+
+SIGNAL CHANGE_ONES		: INTEGER RANGE 0 TO 9;
+SIGNAL CHANGE_TENTHS		: INTEGER RANGE 0 TO 9;
+SIGNAL CHANGE_HUNDREDTHS: INTEGER RANGE 0 TO 9;
 
 --000 CANDY
 --001 CHIPS
@@ -109,123 +127,108 @@ END FUNCTION;
 BEGIN
 
 PROCESS(Clock, Resetn)
-	--integers for doing transaction math
-	VARIABLE PRICE 				: INTEGER RANGE 0 TO 995;
-	VARIABLE TENDER				: INTEGER RANGE 0 TO 995 := 0;
-	VARIABLE CHANGE				: INTEGER RANGE 0 TO 995;
-	
-	VARIABLE PRICE_ONES			: INTEGER RANGE 0 TO 9;
-	VARIABLE PRICE_TENTHS		: INTEGER RANGE 0 TO 9;
-	VARIABLE PRICE_HUNDREDTHS	: INTEGER RANGE 0 TO 9;
-	
-	VARIABLE TENDER_ONES		: INTEGER RANGE 0 TO 9;
-	VARIABLE TENDER_TENTHS		: INTEGER RANGE 0 TO 9;
-	VARIABLE TENDER_HUNDREDTHS: INTEGER RANGE 0 TO 9;
-	
-	VARIABLE CHANGE_ONES		: INTEGER RANGE 0 TO 9;
-	VARIABLE CHANGE_TENTHS		: INTEGER RANGE 0 TO 9;
-	VARIABLE CHANGE_HUNDREDTHS: INTEGER RANGE 0 TO 9;
-
 	BEGIN 
-		
-		IF Resetn = '0' THEN
+		IF Resetn = '1' THEN
 			STATE <= STATE0;
+			TENDER <= 0;
 		ELSIF (Rising_Edge(Clock)) THEN
 			CASE STATE IS
 				WHEN STATE0 =>
-					TENDER := 0;
-					IF (Next_State = '1') THEN
+					STATE0_LED <= '1';
+					STATE1_LED <= '0';
+					STATE2_LED <= '0';
+					IF (Next_State = '0') THEN
 						STATE <= STATE1;
-					END IF;
-					--First Column	
-					IF (Row = "000" and Col = "00") THEN    --SLOT A0
-						Item_Slot <= "00000"; --M AND M'S
-						CATEGORY <= "000";
-					ELSIF (Row = "001" and Col = "00") THEN --SLOT B0
-						Item_Slot <= "00001"; --BBQ CHIPS 
-						CATEGORY <= "001";
-					ELSIF (Row = "010" and Col = "00") THEN --SLOT C0
-						Item_Slot <= "00010"; --COOL RANCH DORITOS
-						CATEGORY <= "001";
-					ELSIF (Row = "011" and Col = "00") THEN --SLOT D0
-						Item_Slot <= "00011"; --ROASTED ALMOND NUTS
-						CATEGORY <= "010";
-					ELSIF (Row = "100" and Col = "00") THEN --SLOT E0
-						Item_Slot <= "00100"; --COCA COLA
-						CATEGORY <= "011";
-					ELSIF (Row = "101" and Col = "00") THEN --SLOT F0
-						Item_Slot <= "00101"; --DIET MOUNTAIN DEW
-						CATEGORY <= "100";
-					ELSIF (Row = "110" and Col = "00") THEN --SLOT G0
-						Item_Slot <= "00110"; --FRUIT PUNCH
-						CATEGORY <= "101";
-						--Second Column
-					ELSIF (Row = "000" and Col = "01") THEN --SLOT A1
-						Item_Slot <= "01000"; --SKITTLES
-						CATEGORY <= "000";
-					ELSIF (Row = "001" and Col = "01") THEN --SLOT B1
-						Item_Slot <= "01001"; --SOUR CREAM & ONION CHIPS
-						CATEGORY <= "001";
-					ELSIF (Row = "010" and Col = "01") THEN --SLOT C1
-						Item_Slot <= "01010"; --SPICY NACHO DORITOS
-						CATEGORY <= "001";
-					ELSIF (Row = "011" and Col = "01") THEN --SLOT D1
-						Item_Slot <= "01011"; --TRAIL MIX
-						CATEGORY <= "010";	
-					ELSIF (Row = "100" and Col = "01") THEN --SLOT E1
-						Item_Slot <= "01100"; --SPRITE
-						CATEGORY <= "011";
-					ELSIF (Row = "101" and Col = "01") THEN --SLOT F1
-						Item_Slot <= "01101"; --DIET COKE
-						CATEGORY <= "100";
-					ELSIF (Row = "110" and Col = "01") THEN --SLOT G1
-						Item_Slot <= "01110"; --LEMONADE
-						CATEGORY <= "101";
-						--Third Column
-					ELSIF (Row = "000" and Col = "10") THEN --SLOT A2
-						Item_Slot <= "10000"; --HERSHEYS CHOCOLATE BAR	
-						CATEGORY <= "000";
-					ELSIF (Row = "001" and Col = "10") THEN --SLOT B2
-						Item_Slot <= "10001"; --SALT & VINEGAR CHIPS
-						CATEGORY <= "001";
-					ELSIF (Row = "010" and Col = "10") THEN --SLOT C2
-						Item_Slot <= "10010"; --SWEET CHILI DORITOS
-						CATEGORY <= "001";
-					ELSIF (Row = "011" and Col = "10") THEN --SLOT D2
-						Item_Slot <= "10011"; --GRONOLA BAR
-						CATEGORY <= "010";
-					ELSIF (Row = "100" and Col = "10") THEN --SLOT E2
-						Item_Slot <= "10100"; --DR PEPPER
-						CATEGORY <= "011";
-					ELSIF (Row = "101" and Col = "10") THEN --SLOT F2
-						Item_Slot <= "10101"; --DIET DR PEPPER
-						CATEGORY <= "100";
-					ELSIF (Row = "110" and Col = "10") THEN --SLOT G2
-						Item_Slot <= "10110"; --APPLE JUICE 
-						CATEGORY <= "101";
-						--Fourth Column 
-					ELSIF (Row = "000" and Col = "11") THEN --SLOT A3
-						Item_Slot <= "11000"; --PROTEIN BAR 
-						CATEGORY <= "000";
-					ELSIF (Row = "001" and Col = "11") THEN --SLOT B3
-						Item_Slot <= "11001"; --CLASSIC POTATO CHIPS
-						CATEGORY <= "001";
-					ELSIF (Row = "010" and Col = "11") THEN --SLOT C3
-						Item_Slot <= "11010"; --NACHO CHEESE DORITOS
-						CATEGORY <= "001";
-					ELSIF (Row = "011" and Col = "11") THEN --SLOT D3
-						Item_Slot <= "11011"; --SALTED PEANUTS
-						CATEGORY <= "010";
-					ELSIF (Row = "100" and Col = "11") THEN --SLOT E3
-						Item_Slot <= "11100"; --ORANGE FANTA
-						CATEGORY <= "011";	
-					ELSIF (Row = "101" and Col = "11") THEN --SLOT F3
-						Item_Slot <= "11101"; --DIET SPRITE
-						CATEGORY <= "100";
-					ELSIF (Row = "100" and Col = "11") THEN --SLOT G3
-						Item_Slot <= "11110"; --ORANGE JUICE
-						CATEGORY <= "101";
-					END IF;
+					ELSE
+						--First Column	
+						IF (Row = "000" and Col = "00") THEN    --SLOT A0
+							Item_Slot <= "00000"; --M AND M'S
+							CATEGORY <= "000";
+						ELSIF (Row = "001" and Col = "00") THEN --SLOT B0
+							Item_Slot <= "00001"; --BBQ CHIPS 
+							CATEGORY <= "001";
+						ELSIF (Row = "010" and Col = "00") THEN --SLOT C0
+							Item_Slot <= "00010"; --COOL RANCH DORITOS
+							CATEGORY <= "001";
+						ELSIF (Row = "011" and Col = "00") THEN --SLOT D0
+							Item_Slot <= "00011"; --ROASTED ALMOND NUTS
+							CATEGORY <= "010";
+						ELSIF (Row = "100" and Col = "00") THEN --SLOT E0
+							Item_Slot <= "00100"; --COCA COLA
+							CATEGORY <= "011";
+						ELSIF (Row = "101" and Col = "00") THEN --SLOT F0
+							Item_Slot <= "00101"; --DIET MOUNTAIN DEW
+							CATEGORY <= "100";
+						ELSIF (Row = "110" and Col = "00") THEN --SLOT G0
+							Item_Slot <= "00110"; --FRUIT PUNCH
+							CATEGORY <= "101";
+							--Second Column
+						ELSIF (Row = "000" and Col = "01") THEN --SLOT A1
+							Item_Slot <= "01000"; --SKITTLES
+							CATEGORY <= "000";
+						ELSIF (Row = "001" and Col = "01") THEN --SLOT B1
+							Item_Slot <= "01001"; --SOUR CREAM & ONION CHIPS
+							CATEGORY <= "001";
+						ELSIF (Row = "010" and Col = "01") THEN --SLOT C1
+							Item_Slot <= "01010"; --SPICY NACHO DORITOS
+							CATEGORY <= "001";
+						ELSIF (Row = "011" and Col = "01") THEN --SLOT D1
+							Item_Slot <= "01011"; --TRAIL MIX
+							CATEGORY <= "010";	
+						ELSIF (Row = "100" and Col = "01") THEN --SLOT E1
+							Item_Slot <= "01100"; --SPRITE
+							CATEGORY <= "011";
+						ELSIF (Row = "101" and Col = "01") THEN --SLOT F1
+							Item_Slot <= "01101"; --DIET COKE
+							CATEGORY <= "100";
+						ELSIF (Row = "110" and Col = "01") THEN --SLOT G1
+							Item_Slot <= "01110"; --LEMONADE
+							CATEGORY <= "101";
+							--Third Column
+						ELSIF (Row = "000" and Col = "10") THEN --SLOT A2
+							Item_Slot <= "10000"; --HERSHEYS CHOCOLATE BAR	
+							CATEGORY <= "000";
+						ELSIF (Row = "001" and Col = "10") THEN --SLOT B2
+							Item_Slot <= "10001"; --SALT & VINEGAR CHIPS
+							CATEGORY <= "001";
+						ELSIF (Row = "010" and Col = "10") THEN --SLOT C2
+							Item_Slot <= "10010"; --SWEET CHILI DORITOS
+							CATEGORY <= "001";
+						ELSIF (Row = "011" and Col = "10") THEN --SLOT D2
+							Item_Slot <= "10011"; --GRONOLA BAR
+							CATEGORY <= "010";
+						ELSIF (Row = "100" and Col = "10") THEN --SLOT E2
+							Item_Slot <= "10100"; --DR PEPPER
+							CATEGORY <= "011";
+						ELSIF (Row = "101" and Col = "10") THEN --SLOT F2
+							Item_Slot <= "10101"; --DIET DR PEPPER
+							CATEGORY <= "100";
+						ELSIF (Row = "110" and Col = "10") THEN --SLOT G2
+							Item_Slot <= "10110"; --APPLE JUICE 
+							CATEGORY <= "101";
+							--Fourth Column 
+						ELSIF (Row = "000" and Col = "11") THEN --SLOT A3
+							Item_Slot <= "11000"; --PROTEIN BAR 
+							CATEGORY <= "000";
+						ELSIF (Row = "001" and Col = "11") THEN --SLOT B3
+							Item_Slot <= "11001"; --CLASSIC POTATO CHIPS
+							CATEGORY <= "001";
+						ELSIF (Row = "010" and Col = "11") THEN --SLOT C3
+							Item_Slot <= "11010"; --NACHO CHEESE DORITOS
+							CATEGORY <= "001";
+						ELSIF (Row = "011" and Col = "11") THEN --SLOT D3
+							Item_Slot <= "11011"; --SALTED PEANUTS
+							CATEGORY <= "010";
+						ELSIF (Row = "100" and Col = "11") THEN --SLOT E3
+							Item_Slot <= "11100"; --ORANGE FANTA
+							CATEGORY <= "011";	
+						ELSIF (Row = "101" and Col = "11") THEN --SLOT F3
+							Item_Slot <= "11101"; --DIET SPRITE
+							CATEGORY <= "100";
+						ELSIF (Row = "100" and Col = "11") THEN --SLOT G3
+							Item_Slot <= "11110"; --ORANGE JUICE
+							CATEGORY <= "101";
+						END IF;
 					
 					CASE Col IS
 						WHEN "00" => TEMP_HEX_COL <= HEX_ZERO;
@@ -252,73 +255,137 @@ PROCESS(Clock, Resetn)
 					--011 SODA
 					--100 DIET SODA
 					--101 JUICE 
-					CASE CATEGORY IS
-						WHEN "000" => PRICE := PRICE_CANDY;
-						WHEN "001" => PRICE := PRICE_CHIPS;
-						WHEN "010" => PRICE := PRICE_NUTS;
-						WHEN "011" => PRICE := PRICE_SODA;
-						WHEN "100" => PRICE := PRICE_DIETSODA;
-						WHEN "101" => PRICE := PRICE_JUICE;
-						WHEN OTHERS => PRICE := 0;
-					END CASE;
+					
 					
 					--grab each digit of PRICE
-					PRICE_ONES := PRICE / 100;
-					PRICE_TENTHS := (PRICE MOD 100) / 10;
-					PRICE_HUNDREDTHS := PRICE MOD 10;
 					--convert each digit to hex and send to 7 segment display
-					TEMP_HEX_COUNTER1 <= convertToHex(PRICE_ONES);
-					TEMP_HEX_COUNTER2 <= convertToHex(PRICE_TENTHS);
-					TEMP_HEX_COUNTER3 <= convertToHex(PRICE_HUNDREDTHS);
+					--TEMP_HEX_COUNTER1 <= convertToHex(PRICE_ONES);
+					--TEMP_HEX_COUNTER2 <= convertToHex(PRICE_TENTHS);
+					--TEMP_HEX_COUNTER3 <= convertToHex(PRICE_HUNDREDTHS);
+					END IF;
 					
 				WHEN STATE1 => 
-					IF (Next_State = '1') THEN
+					STATE0_LED <= '0';
+					STATE1_LED <= '1';
+					STATE2_LED <= '0';
+					IF (Next_State = '0') THEN
 						STATE <= STATE2;
-					END IF;
-					IF (Adjust_Up = '1' AND Adjust_Down = '0') THEN
-						IF (TENDER < 995) THEN
-							TENDER := TENDER + 5;
+					ELSE
+						IF (Adjust_Up = '0' AND Adjust_Down = '1') THEN
+							IF (TENDER < 995) THEN
+								TENDER <= TENDER + 5;
+							END IF;
+							
+						ELSIF (Adjust_Down = '1' AND Adjust_Down = '0') THEN
+							IF (TENDER >= 5) THEN
+								TENDER <= TENDER - 5;
+							END IF;
 						END IF;
 						
-					ELSIF (Adjust_Down = '1' AND Adjust_Down = '0') THEN
-						IF (TENDER >= 5) THEN
-							TENDER := TENDER - 5;
+						--grab each digit of TENDER
+						--TENDER_ONES <= TENDER / 100;
+						--TENDER_TENTHS <= (TENDER MOD 100) / 10;
+						--TENDER_HUNDREDTHS <= TENDER MOD 10;
+						--convert each digit to hex and send to 7 segment display
+						--TEMP_HEX_COUNTER1 <= convertToHex(TENDER_ONES);
+						--TEMP_HEX_COUNTER2 <= convertToHex(TENDER_TENTHS);
+						--TEMP_HEX_COUNTER3 <= convertToHex(TENDER_HUNDREDTHS);
+					END IF;
+				WHEN STATE2 =>
+					STATE0_LED <= '0';
+					STATE1_LED <= '0';
+					STATE2_LED <= '1';
+					IF (Next_State = '0') THEN
+						STATE <= STATE0;
+					ELSE
+						IF (TENDER >= PRICE) THEN
+							CHANGE <= TENDER - PRICE;
+					
+							--CHANGE_ONES <= CHANGE / 100;
+							--CHANGE_TENTHS <= (CHANGE MOD 100) / 10;
+							--CHANGE_HUNDREDTHS <= CHANGE MOD 10;
+							--convert each digit to hex and send to 7 segment display
+							--TEMP_HEX_COUNTER1 <= convertToHex(CHANGE_ONES);
+							--TEMP_HEX_COUNTER2 <= convertToHex(CHANGE_TENTHS);
+							--TEMP_HEX_COUNTER3 <= convertToHex(CHANGE_HUNDREDTHS);
 						END IF;
 					END IF;
-					
-					--grab each digit of TENDER
-					TENDER_ONES := TENDER / 100;
-					TENDER_TENTHS := (TENDER MOD 100) / 10;
-					TENDER_HUNDREDTHS := TENDER MOD 10;
-					--convert each digit to hex and send to 7 segment display
-					TEMP_HEX_COUNTER1 <= convertToHex(TENDER_ONES);
-					TEMP_HEX_COUNTER2 <= convertToHex(TENDER_TENTHS);
-					TEMP_HEX_COUNTER3 <= convertToHex(TENDER_HUNDREDTHS);
-				WHEN STATE2 =>
-					IF (Next_State = '1') THEN
-						STATE <= STATE0;
-					END IF;
-					
-					CHANGE := TENDER - PRICE;
-					
-					CHANGE_ONES := CHANGE / 100;
-					CHANGE_TENTHS := (CHANGE MOD 100) / 10;
-					CHANGE_HUNDREDTHS := CHANGE MOD 10;
-					--convert each digit to hex and send to 7 segment display
-					TEMP_HEX_COUNTER1 <= convertToHex(CHANGE_ONES);
-					TEMP_HEX_COUNTER2 <= convertToHex(CHANGE_TENTHS);
-					TEMP_HEX_COUNTER3 <= convertToHex(CHANGE_HUNDREDTHS);
 			END CASE;	
-					
 		END IF;
 	
 	HEX3 <= HEX_OFF;
 		
 END PROCESS;
+	
+	PRICE_ONES <= PRICE_CANDY / 100 WHEN CATEGORY = "000" ELSE
+			 PRICE_CHIPS / 100 WHEN CATEGORY = "001" ELSE
+			 PRICE_NUTS / 100 WHEN CATEGORY = "010" ELSE
+			 PRICE_SODA / 100 WHEN CATEGORY = "011" ELSE
+			 PRICE_DIETSODA / 100 WHEN CATEGORY = "100" ELSE
+			 0;
+	PRICE_TENTHS <= (PRICE_CANDY MOD 100) / 10 WHEN CATEGORY = "000" ELSE
+			 (PRICE_CHIPS MOD 100) / 10 WHEN CATEGORY = "001" ELSE
+			 (PRICE_NUTS MOD 100) / 10 WHEN CATEGORY = "010" ELSE
+			 (PRICE_SODA MOD 100)/ 10 WHEN CATEGORY = "011" ELSE
+			 (PRICE_DIETSODA MOD 100) / 10 WHEN CATEGORY = "100" ELSE
+			 0;
+	PRICE_HUNDREDTHS <= (PRICE_CANDY MOD 10) WHEN CATEGORY = "000" ELSE
+			 (PRICE_CHIPS MOD 10) WHEN CATEGORY = "001" ELSE
+			 (PRICE_NUTS MOD 10) WHEN CATEGORY = "010" ELSE
+			 (PRICE_SODA MOD 10) WHEN CATEGORY = "011" ELSE
+			 (PRICE_DIETSODA MOD 10) WHEN CATEGORY = "100" ELSE
+			 0;
+			 
+	TENDER_ONES <= TENDER / 100 WHEN STATE = STATE1 ELSE
+			 TENDER / 100 WHEN STATE = STATE1 ELSE
+			 TENDER / 100 WHEN STATE = STATE1 ELSE
+			 TENDER / 100 WHEN STATE = STATE1 ELSE
+			 TENDER / 100 WHEN STATE = STATE1 ELSE
+			 0;
+	TENDER_TENTHS <= (TENDER MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 100)/ 10 WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 0;
+	TENDER_HUNDREDTHS <= (TENDER MOD 10) WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 10) WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 10) WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 10) WHEN STATE = STATE1 ELSE
+			 (TENDER MOD 10) WHEN STATE = STATE1 ELSE
+			 0;
+	
+	CHANGE_ONES <= (TENDER - PRICE) / 100 WHEN STATE = STATE1 ELSE
+			 (TENDER - PRICE) / 100 WHEN STATE = STATE1 ELSE
+			 (TENDER - PRICE) / 100 WHEN STATE = STATE1 ELSE
+			 (TENDER - PRICE) / 100 WHEN STATE = STATE1 ELSE
+			 (TENDER - PRICE) / 100 WHEN STATE = STATE1 ELSE
+			 0;
+	CHANGE_TENTHS <= ((TENDER - PRICE) MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 100)/ 10 WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 100) / 10 WHEN STATE = STATE1 ELSE
+			 0;
+	CHANGE_HUNDREDTHS <= ((TENDER - PRICE) MOD 10) WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 10) WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 10) WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 10) WHEN STATE = STATE1 ELSE
+			 ((TENDER - PRICE) MOD 10) WHEN STATE = STATE1 ELSE
+			 0;
 
-	HEX_COUNTER1 <= TEMP_HEX_COUNTER1;
-	HEX_COUNTER2 <= TEMP_HEX_COUNTER2;
-	HEX_COUNTER3 <= TEMP_HEX_COUNTER3;
+	HEX_COUNTER1 <= convertToHex(PRICE_ONES) WHEN STATE = STATE0 ELSE
+						convertToHex(TENDER_ONES) WHEN STATE = STATE1 ELSE
+						convertToHex(CHANGE_ONES) WHEN STATE = STATE2 ELSE
+						"1111111";	
+	HEX_COUNTER2 <= convertToHex(PRICE_TENTHS) WHEN STATE = STATE0 ELSE
+						convertToHex(TENDER_TENTHS) WHEN STATE = STATE1 ELSE
+						convertToHex(CHANGE_TENTHS) WHEN STATE = STATE2 ELSE
+						"1111111";
+	HEX_COUNTER3 <= convertToHex(PRICE_HUNDREDTHS) WHEN STATE = STATE0 ELSE
+						convertToHex(TENDER_HUNDREDTHS) WHEN STATE = STATE1 ELSE
+						convertToHex(CHANGE_HUNDREDTHS) WHEN STATE = STATE2 ELSE
+						"1111111";	
 
 	HEX_ROW <= TEMP_HEX_ROW;
 	HEX_COL <= TEMP_HEX_COL;
